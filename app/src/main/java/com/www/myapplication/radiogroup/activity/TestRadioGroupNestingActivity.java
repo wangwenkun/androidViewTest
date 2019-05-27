dc5626db2b8f5c59527881171b2f890ba6babaf1
wangwenkun <wangwenkun192@163.com>
2019-05-27 15:58:25 +0800

    3

---
+++ app/src/main/java/com/www/myapplication/radiogroup/activity/TestRadioGroupNestingActivity.java
@@ -0,0 +1,14 @@
+package com.www.myapplication.radiogroup.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.www.myapplication.R;
+
+public class TestRadioGroupNestingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_radio_group_nexting);
+    }
+}
